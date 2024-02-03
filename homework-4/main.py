f2630949812524b2d399d1045235cd7d40918257
Vladimir Nikitin <vanjilbroolkyn@gmail.com>
2024-02-03 12:33:01 -0500

    реализовал класс PlayList

--- homework-4/main.py
+++ homework-4/main.py
@@ -1,11 +1,15 @@
-from src.video import Video, PLVideo
+from src.video import Video, PLVideo, PlayList
 
 if __name__ == '__main__':
+    video1 = Video('AWX4JnAnjBE', 'GIL в Python: зачем он нужен и как с этим жить', 'https://www.youtube.com/watch?v=AWX4JnAnjBE', 1000, 50)
+    video2 = PLVideo('4fObz_qw9u4', 'PLv_zOGKKxVph_8g2Mqc3LMhj0M_BfasbC', 'MoscowPython Meetup 78 - вступление', 'https://www.youtube.com/watch?v=4fObz_qw9u4', 500, 30)
 
-    video1 = Video('AWX4JnAnjBE', 'GIL в Python: зачем он нужен и как с этим жить',
-                   'https://www.youtube.com/watch?v=AWX4JnAnjBE', 1000, 50)
-    video2 = PLVideo('4fObz_qw9u4', 'PLv_zOGKKxVph_8g2Mqc3LMhj0M_BfasbC', 'MoscowPython Meetup 78 - вступление',
-                     'https://www.youtube.com/watch?v=4fObz_qw9u4', 500, 30)
+    playlist = PlayList('PLv_zOGKKxVph_8g2Mqc3LMhj0M_BfasbC', 'Python Meetup Playlist', 'https://www.youtube.com/playlist?list=PLv_zOGKKxVph_8g2Mqc3LMhj0M_BfasbC')
+    playlist.add_video(video1)
+    playlist.add_video(video2)
 
+    # Проверяем вывод
     assert str(video1) == 'GIL в Python: зачем он нужен и как с этим жить'
     assert str(video2) == 'MoscowPython Meetup 78 - вступление'
+    assert playlist.total_duration.total_seconds() == 1500  # сумма длительностей видео
+    assert playlist.show_best_video() == 'https://www.youtube.com/watch?v=AWX4JnAnjBE'  # ссылка на самое популярное видео
--- src/video.py
+++ src/video.py
@@ -1,4 +1,24 @@
-# src/video.py
+from datetime import timedelta
+
+class PlayList:
+    def __init__(self, playlist_id, title, playlist_url):
+        self.playlist_id = playlist_id
+        self.title = title
+        self.playlist_url = playlist_url
+        self.videos = []
+
+    def add_video(self, video):
+        self.videos.append(video)
+
+    @property
+    def total_duration(self):
+        total_duration_seconds = sum(video.views for video in self.videos)
+        return timedelta(seconds=total_duration_seconds)
+
+    def show_best_video(self):
+        best_video = max(self.videos, key=lambda video: video.likes)
+        return best_video.video_url
+
 
 class Video:
     def __init__(self, video_id, title, video_url, views, likes):
@@ -17,13 +37,14 @@ class PLVideo(Video):
         super().__init__(video_id, title, video_url, views, likes)
         self.playlist_id = playlist_id
 
-# Пример использования классов:
 
 if __name__ == '__main__':
-    # Создаем два экземпляра класса
+
     video1 = Video('AWX4JnAnjBE', 'GIL в Python: зачем он нужен и как с этим жить', 'https://www.youtube.com/watch?v=AWX4JnAnjBE', 1000, 50)
     video2 = PLVideo('4fObz_qw9u4', 'PLv_zOGKKxVph_8g2Mqc3LMhj0M_BfasbC', 'MoscowPython Meetup 78 - вступление', 'https://www.youtube.com/watch?v=4fObz_qw9u4', 500, 30)
 
     # Проверяем вывод
     assert str(video1) == 'GIL в Python: зачем он нужен и как с этим жить'
     assert str(video2) == 'MoscowPython Meetup 78 - вступление'
+
+
