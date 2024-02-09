8fd81c62cf17a9b7f4162952a96b9dea7526a3cd
Vladimir Nikitin <vanjilbroolkyn@gmail.com>
2024-02-09 11:25:14 -0500

    внедрил Try-Except

--- homework-6/main.py
+++ homework-6/main.py
@@ -1,6 +1,9 @@
 from src.video import Video
 
 if __name__ == '__main__':
-    broken_video = Video('broken_video_id')
-    assert broken_video.title is None
-    assert broken_video.like_count is None
+    try:
+        broken_video = Video('broken_video_id')
+        assert broken_video.title is None
+        assert broken_video.like_count is None
+    except Exception as e:
+        print(f"An error occurred: {e}")
